575d93b1123108338d3a9658b032f99610645d48
simplyahmazing <ahmad19526@gmail.com>
2017-10-27 10:20:09 -0400

    setup sample package json from zulip project

--- dist/init.js
+++ dist/init.js
@@ -1,10 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var fs = require("fs");
-function writePublishSection(publishInfo, packageJsonPath) {
-    var packageJson = JSON.parse(fs.readFileSync(packageJsonPath).toString());
+var detectIndent = require("detect-indent");
+function writePublishSection(packageJsonPath, publishInfo) {
+    var file = fs.readFileSync(packageJsonPath).toString();
+    var indent = detectIndent(file).indent;
+    var packageJson = JSON.parse(file);
     packageJson.build.publish = publishInfo;
-    fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, ' '));
+    fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, indent));
 }
 exports.writePublishSection = writePublishSection;
 function init(packageJsonPath, s3Bucket) {
@@ -13,6 +16,6 @@ function init(packageJsonPath, s3Bucket) {
         bucket: s3Bucket,
         path: '${env.BRANCH}/${env.COMMIT}/${env.PLATFORM}'
     };
-    writePublishSection(publishInfo, process.env);
+    writePublishSection(packageJsonPath, publishInfo);
 }
 exports.init = init;
--- src/init.ts
+++ src/init.ts
@@ -1,12 +1,15 @@
 import * as fs from 'fs'
 import { spawn } from 'child_process'
+import * as detectIndent from 'detect-indent'
 
 import * as fission from './index'
 
-export function writePublishSection (publishInfo : Object, packageJsonPath : string) {
-  const packageJson = JSON.parse(fs.readFileSync(packageJsonPath).toString())
+export function writePublishSection (packageJsonPath : string, publishInfo : Object) {
+  const file = fs.readFileSync(packageJsonPath).toString()
+  const indent = detectIndent(file).indent 
+  const packageJson = JSON.parse(file)
   packageJson.build.publish = publishInfo
-  fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, ' '))
+  fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, indent))
 }
 
 export function init (packageJsonPath : string, s3Bucket : string) {
@@ -15,5 +18,5 @@ export function init (packageJsonPath : string, s3Bucket : string) {
     bucket: s3Bucket,
     path: '${env.BRANCH}/${env.COMMIT}/${env.PLATFORM}'
   }
-  writePublishSection(publishInfo, process.env)
+  writePublishSection(packageJsonPath, publishInfo)
 }
