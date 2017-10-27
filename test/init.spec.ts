5de860b9ca5af05e3415b2ec2ef2d7a14fdb408f
simplyahmazing <ahmad19526@gmail.com>
2017-10-27 10:47:00 -0400

    write test for init on existin package.json

--- test/init.spec.ts
+++ test/init.spec.ts
@@ -8,9 +8,6 @@ import { writePublishSection } from '../src/init'
 
 
 describe('fission init', () => {
-  // beforeEach(() => {
-  // })
-
   it('initalizes empty package.json', () => {
     const emptyPackageJson = path.join(__dirname, 'data', 'empty-package.json')
     const emptyTestPackageJson = path.join(os.tmpdir(), 'empty-package.json')
@@ -30,6 +27,20 @@ describe('fission init', () => {
   })
 
   it('initalizes existing package.json', () => {
-    // expect(result).to.equal(true)
+    const existingPackageJson = path.join(__dirname, 'data', 'existing-package.json')
+    const existingTestPackageJson = path.join(os.tmpdir(), 'existing-package.json')
+    // copy test file to temp dir
+    fs.writeFileSync(existingTestPackageJson, fs.readFileSync(existingPackageJson))
+
+    const publishInfo = {
+      provider: "s3",
+      bucket: "sample-bucket",
+      path: '${env.BRANCH}/${env.COMMIT}/${env.PLATFORM}'
+    }
+
+    writePublishSection(existingTestPackageJson, publishInfo)
+    const result = fs.readFileSync(existingTestPackageJson)
+    const expected = fs.readFileSync(path.join(__dirname, 'data', 'res-existing-package.json'))
+    expect(JSON.parse(result.toString())).to.deep.equal(JSON.parse(expected.toString()))
   })
 })
